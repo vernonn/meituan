dff3c71e1377c2c63fc6dd8a425f6687be9f5a2c
vernonn <695035427@qq.com>
2019-02-20 14:42:00 +0800

    store new

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,7 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        meituan-app
-      </h1>
-      <h2 class="subtitle">
-        My luminous Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div>
+    
+  </div>
 </template>
 
 <script>
