e1732d148da3ebcdd1ade13fb50235db0eb4b4e3
vernonn <695035427@qq.com>
2019-03-04 12:16:14 +0800

    service

---
+++ server/dbs/config.js
@@ -0,0 +1,32 @@
+export default {
+  dbs: 'mongodb://127.0.0.1:27017/meituan', // 27017是默认端口，不建议修改
+  redis: {
+    get host() {
+      return '127.0.0.1'
+    },
+    get port() {
+      return 6379 // 6379是默认端口，不建议修改
+    }
+  },
+  smtp: {
+    get host() {
+      return 'smtp.qq.com'
+    },
+    get user() {
+      return '695035427@qq.com'
+    },
+    get pass() {
+      return 'oqznghnlowzebdai'
+    }
+  },
+  get code() {
+    return () => {
+      return Math.random().toString(16).slice(2, 6).toUpperCase()
+    }
+  },
+  get expire() {
+    return () => {
+      return new Date().getTime() + 60 * 60 * 1000
+    }
+  }
+}
