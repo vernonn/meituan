1c77a6a3b5e31c306f8cb54131b9df6c4ae83e72
vernonn <695035427@qq.com>
2019-03-11 16:09:14 +0800

    login and exit

--- components/public/header/user.vue
+++ components/public/header/user.vue
@@ -4,7 +4,7 @@
       欢迎您，<span class="username">{{ user }}</span>
       [<nuxt-link to="/exit">退出</nuxt-link>]
     </template>
-    <template>
+    <template v-else>
       <nuxt-link to="/login" class="login">立即登录</nuxt-link>
       <nuxt-link to="/register" class="register">注册</nuxt-link>
     </template>
@@ -17,7 +17,13 @@ export default {
     return {
       user: ''
     }
-  }
+  },
+  async mounted() {
+    const {status, data: {user}} = await this.$axios.get('/users/getUser')
+    if (status === 200) {
+      this.user = decodeURIComponent(user) // 有中文账号，需要decode
+    }
+  },
 }
 </script>
 
--- pages/exit.vue
+++ pages/exit.vue
@@ -1,10 +1,17 @@
 <template>
-  
+  <div class=""></div>
 </template>
 
 <script>
 export default {
-  layout: 'blank'
+  layout: 'blank',
+  // 使用中间件，到达/exit页面自动执行
+  middleware: async (ctx) => {
+    let {status, data} = await ctx.$axios.get('/users/exit')
+    if (status === 200 && data.code === 0) {
+      window.location.href = '/'
+    }
+  }
 }
 </script>
 
