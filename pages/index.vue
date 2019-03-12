37f7fbdf80f3c9e9c637c0874cbe240a0b9f9110
vernonn <695035427@qq.com>
2019-03-12 15:50:55 +0800

    life slider

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,9 @@
       <el-col :span="5">
         <main-menu/>
       </el-col>
-      <el-col :span="19"></el-col>
+      <el-col :span="19">
+        <life/>
+      </el-col>
     </el-row>
     <el-row :span="24">
       
@@ -14,9 +16,11 @@
 
 <script>
 import MainMenu from '@/components/index/menu'
+import Life from '@/components/index/life'
 export default {
   components: {
-    MainMenu
+    MainMenu,
+    Life
   }
 }
 </script>
