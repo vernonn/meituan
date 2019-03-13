fb6b3d3539ebccc64e07d3fa02cdcc3cbc9bafc0
vernonn <695035427@qq.com>
2019-03-13 11:58:07 +0800

    格式化

--- components/changeCity/hot.vue
+++ components/changeCity/hot.vue
@@ -2,29 +2,30 @@
   <div class="m-hcity">
     <dl>
       <dt>热门城市：</dt>
-      <dd
-        v-for="item in list"
-        :key="item.id">{{ item.name==='市辖区'?item.province:item.name }}</dd>
+      <dd v-for="item in list" :key="item.id">{{ item.name==='市辖区'?item.province:item.name }}</dd>
     </dl>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      list:[]
+      list: []
     }
   },
-  async mounted(){
-    let {status,data:{hots}}=await this.$axios.get('/geo/hotCity')
-    if(status===200){
-      this.list=hots
+  async mounted() {
+    let {
+      status,
+      data: { hots }
+    } = await this.$axios.get('/geo/hotCity')
+    if (status === 200) {
+      this.list = hots
     }
   }
 }
 </script>
 
 <style lang="scss">
-  @import "@/assets/css/changeCity/hot.scss";
+@import '@/assets/css/changeCity/hot.scss';
 </style>
